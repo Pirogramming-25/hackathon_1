39efa2fffaa674085a2934d0fd4f87ee3f1b9842
daynight0219-ux <yewongrey@naver.com>
2026-07-14 05:30:38 +0900

    fix/fe:views.py 오타 수정

--- families/views.py
+++ families/views.py
@@ -95,7 +95,7 @@ class FamilyRequestCreateView(APIView):
                 requester=requester,
                 status=FamilyRelation.Status.PENDING,
             )
-            status_code = status.HTTP_201_CREAED
+            status_code = status.HTTP_201_CREATED
 
         return success_response(
             FamilyRelationSerializer(relation, context={"request": request}).data,
